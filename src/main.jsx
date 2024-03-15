d90164d8b9419bdcaf6c38e45c8c5806bcb2eb8c
Remon Tripura <remontripura045@gmail.com>
2024-03-15 09:41:42 +0600

    bug fixed

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,6 @@ import { RouterProvider } from "react-router-dom";
 import router from "./router/router.jsx";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { MetaMaskProvider } from "@metamask/sdk-react";
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
